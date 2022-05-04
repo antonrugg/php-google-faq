9f5185e9425d56d0c053b6b719394bc772973d45
Antonio Ruggiero <antonrugg@gmail.com>
2022-05-04 17:28:51 +0200

    answers printed

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ include('database.php');
 
 foreach($allfaqs as $faq){
         echo('<h2>' . $faq['question'] . '</h2>');
+        foreach($faq['answers'] as $answer){
+            echo('<p>' . $answer . '</p>');
+        }
     } 
     
     
